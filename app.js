86341a4c6a67f16302b09d611a040f8929b3d489
saadali12345 <saad.ali.bscs@gmail.com>
2023-08-22 23:31:28 +0500

    Arrays

---
+++ app.js
@@ -0,0 +1,38 @@
+// Question No 1 
+
+// let studentsnames=["saad","Ahad","obaid",'Ali',"Hamza"];
+// console.log(studentsnames);
+
+
+//Question No 2
+
+// let studentsnames=["charlie",'Eve']
+// studentsnames.push('Nike');
+// console.log(studentsnames);
+
+
+
+// Question No 3
+
+// let studentsnames=[ "Alice", "Bob", "Charlie", "David", "Eve"];
+// studentsnames.splice(3,1)
+// console.log(studentsnames);
+
+// Question No 4
+
+
+// let studentsnames=[ "Alice", "Bob", "Charlie", "David", "Eve"]
+// studentsnames.push('Grace','Helen');
+// console.log(studentsnames);
+
+
+
+// Question No 5
+
+// var studentNames = ["Alice", "Bob", "Charlie", "Eve"];
+// var index = studentNames.indexOf("Bob");
+// studentNames[index] = "saad";
+// console.log(studentNames); 
+
+
+//Question No 6
